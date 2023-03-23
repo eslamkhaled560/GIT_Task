708bab1183438682f10a02d3580a5115765f3243
Islam <eslamkhaled560@gmail.com>
2023-03-23 12:38:38 +0200

    Slight Change

--- code.py
+++ code.py
@@ -2,6 +2,6 @@ print("Hello, World!")
 
 print("First Change")
 
-print("Version 1.0 is tested and ready to be deployed")
-
 print("Remote main branch changes during the develpment of Version 1.0")
+
+print("Version 1.0 is tested and ready to be deployed")
